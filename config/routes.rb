3ffa6d0fbbd4668111ead87896a7068e9e4b8289
Callan Cook <callan.c.cook@gmail.com>
2017-04-04 20:08:38 +0200

    Added landing page, made panding page the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get 'static_pages/products' , :as => 'products_page'
 
   get 'static_pages/index'
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
