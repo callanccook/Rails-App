8f93f5609814ffc4fba21dd3736e19344d471e7d
Callan Cook <callan.c.cook@gmail.com>
2017-04-07 22:53:16 +0200

    4.5 added carousel, created _products file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ product_list = [
 	['Wedding', 'They can be elegant and traditional, or original.', 'http://bellethemagazine.com/wp-content/uploads/2016/04/winter-wedding-cake-3-200x200.jpg', 'White or Cream', 100],
 	['Birthday', 'Birthday cakes are one tier, rounded. Candles not included.', 'http://www.differentdream.com/wp-content/uploads/2009/09/Special-Needs-Billy-birthday-cake-200x200.jpg', 'Any color', 30],
 	['Cupcake', 'Sizes can be small, medium, or large. Price is by the half dozen.', 'http://www.glorioustreats.com/wp-content/uploads/2011/08/Pretty-colored-cupcakes-200x200.jpg', 'Any color', 6],
-	['Square', 'From one to four tiers, they can be centered or off-centered.', 'http://rs679.pbsrc.com/albums/vv152/nonomsarasnomo/square%20cake/goldandivorysquareweddingcake.jpg~c200', 'Any color', 75],
+	['Square', 'From one to four tiers, they can be centered or off-centered.', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQYuOpq7sph1MFzxQy4SD036NLg-6sp-Ok_I3Ye5vq0_hf_HJiK', 'Any color', 75],
 	['Themed', 'Any theme and any shape.', 'https://cdn001.cakecentral.com/gallery/2016/07/200x200_star-wars-3-d-standing-r2d2-cake-990268VXLxq.jpg', 'Any color', 50],
 	['Round', 'Shaped like a circle. Any toppings/flavor', 'http://img-aws.ehowcdn.com/200x200/cme/cme_public_images/www_ehow_com/i.ehow.com/images/a06/jm/o3/put-fondant-round-cake-800x800.jpg', 'Any color', 75],
 ]
