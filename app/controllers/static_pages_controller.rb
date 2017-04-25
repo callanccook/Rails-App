aeda94603ca8d9a3b0a75d5c0a10d583ed9571d3
Callan Cook <callan.c.cook@gmail.com>
2017-04-25 15:20:57 +0200

    installed sendgrid, added to environment.rb to send emails from heroku

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,8 +4,17 @@ class StaticPagesController < ApplicationController
   end
 
   def landing_page
-    @featured_product = Product.first
     @products = Product.limit(3)
   end
+
+  def thank_you
+	  @name = params[:name]
+	  @email = params[:email]
+	  @message = params[:message]
+	  ActionMailer::Base.mail(:from => @email,
+	    :to => 'callan.c.cook@gmail.com',
+	    :subject => "A new contact form message from #{@name}",
+	    :body => @message).deliver_now
+	end
 
 end
