3ca0582b9b069cfa47c394951f75e3e91ed0a73c
Callan Cook <callan.c.cook@gmail.com>
2017-05-11 18:49:06 +0100

    Create comments view

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  resources :products do 
+    resources :comments
+  end
   resources :users
-  resources :products
+
   get 'static_pages/about' , :as => 'about_page'
+
   get 'static_pages/contact' , :as => 'contact_page'
 
   get '/products', to: 'products#index' , :as => 'products_page'
+
   get '/oders', to: 'orders#index', :as => 'orders_page'
 
   get 'static_pages/index'
+
   root 'static_pages#landing_page'
+
   post 'static_pages/thank_you'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :orders, only: [:index, :show, :create, :destroy]
 end
