ea9d1be4738b714464d2a54d9c2d9a50312ec1a9
Callan Cook <callan.c.cook@gmail.com>
2017-05-16 20:12:17 +0100

    Add and style pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,6 +16,8 @@ class ProductsController < ApplicationController
   # GET /products/1.json
   def show
     @comments = @product.comments.order("created_at DESC")
+    @comments = Comment.paginate(:page => params[:page])
+    # or this -> @comments = Comment.all.paginate(page: params[:page], per_page: 3) 
   end
 
   # GET /products/new
