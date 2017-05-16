af041d61a77926107b38991ba2771bc2b12224d5
Callan Cook <callan.c.cook@gmail.com>
2017-05-16 20:52:25 +0100

    Try to fix rating stars in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   # rating stars code
-  # config.serve_static_assets = true
+  config.serve_static_assets = true
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
