5f6a386c403fe56616e835c6953247c00b86e53f
Callan Cook <callan.c.cook@gmail.com>
2017-05-19 17:16:01 +0100

    Add sign up email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,18 @@ class UserMailer < ApplicationMailer
 	default from: "callan.c.cook@gmail.com"
 
 	def contact_form(email, name, message)
-  @message = message
-    mail(:from => email,
+  	@message = message
+    	mail(:from => email,
         :to => 'callan.c.cook@gmail.com',
         :subject => "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+  	@user = user
+  	@appname = "Fancy Cakes"
+  	mail(to: user.email, subject: "Welcome to #{@appname}!")
+	end
+
 
   def thank_you
 	  @name = params[:name]
