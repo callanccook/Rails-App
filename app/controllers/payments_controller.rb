70ac021ffc7c6dee4e9a4859bd9ff0146b856121
Callan Cook <callan.c.cook@gmail.com>
2017-06-08 15:56:26 -0400

    Add payments controller, javascript alerts

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,37 @@
+class PaymentsController < ApplicationController
+
+	def create
+		@product = Product.find(params[:product_id])
+		@user = current_user
+	  token = params[:stripeToken]
+	  # Create the charge on Stripe's servers - this will charge the user's card
+
+	  begin
+	    charge = Stripe::Charge.create(
+	      amount: (@product.price * 100).to_i, # amount in cents, again
+	      currency: "usd",
+	      source: token,
+	      description: params[:stripeEmail],
+	      receipt_email: params[:stripeEmail]
+	    )
+
+	    if charge.paid
+	    	Order.create(
+	    		product_id: @product.id,
+	    		user_id: @user_id,
+	    		total: @product.price,
+	    		)
+	    	flash[:notice] = "Your payment was processed successfully."
+	    end
+
+	  rescue Stripe::CardError => e
+	    # The card has been declined
+	    body = e.json_body
+    	err = body[:error]
+    	flash[:notice] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+	  end
+	  redirect_to product_path(@product)
+
+	end
+
+end
