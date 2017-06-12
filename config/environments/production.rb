94aa2cc43bce3cb0dcad45ff4e938ec854001652
Callan Cook <callan.c.cook@gmail.com>
2017-06-12 13:53:28 -0400

    Add byebug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  
 
   # rating stars code
   config.serve_static_assets = true
