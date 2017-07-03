dd264c364baebbc9c6c83f8feeb6267b432e17ce
Callan Cook <callan.c.cook@gmail.com>
2017-07-03 15:45:44 -0500

    Altered stripe redirect path

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -30,8 +30,7 @@ class PaymentsController < ApplicationController
     	err = body[:error]
     	flash[:notice] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
 	  end
-	  redirect_to product_path(@product)
-
+	  redirect_to root_path
 	end
 
 end
